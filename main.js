438fc78c849a7e6320062382fe56f4d0f35b0edd
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-04 16:55:31 -0300

    upgrade js in mobile web page

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const ham = document.querySelector('.tools')
 const header = document.querySelector('.header2')
+const closeX = document.querySelector('.close_window')
+const logo = document.querySelector('.logo')
 
 
 ham.addEventListener('click', () => {
-    header.classList.toggle('header2_activate')
+    header.classList.toggle('activate')
+    ham.classList.toggle('delete')
+    closeX.classList.toggle('activate')
+    logo.classList.toggle('desactivate')
   }
 );
+
+document.querySelectorAll('.menu_section').forEach
+    (n => n.addEventListener('click', () => {
+        header.classList.remove('activate')
+        ham.classList.remove('delete')
+        closeX.classList.remove('activate')
+        logo.classList.remove('desactivate')
+    }))
