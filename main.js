1a6660143e6d605936bbe2634604e15245091c29
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-04 16:58:52 -0300

    fix linter error

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-const ham = document.querySelector('.tools')
-const header = document.querySelector('.header2')
-const closeX = document.querySelector('.close_window')
-const logo = document.querySelector('.logo')
+const ham = document.querySelector('.tools');
+const header = document.querySelector('.header2');
+const closeX = document.querySelector('.close_window');
+const logo = document.querySelector('.logo');
 
 
 ham.addEventListener('click', () => {
-    header.classList.toggle('activate')
-    ham.classList.toggle('delete')
-    closeX.classList.toggle('activate')
-    logo.classList.toggle('desactivate')
+  header.classList.toggle('activate');
+  ham.classList.toggle('delete');
+  closeX.classList.toggle('activate');
+  logo.classList.toggle('desactivate');
   }
 );
 
 document.querySelectorAll('.menu_section').forEach
-    (n => n.addEventListener('click', () => {
-        header.classList.remove('activate')
-        ham.classList.remove('delete')
-        closeX.classList.remove('activate')
-        logo.classList.remove('desactivate')
-    }))
+  (n => n.addEventListener('click', () => {
+    header.classList.remove('activate');
+    ham.classList.remove('delete');
+    closeX.classList.remove('activate');
+    logo.classList.remove('desactivate');
+    }));
+    
