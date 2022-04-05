e777b5441b5a3b907805e30e9acb5e9186973611
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-05 19:57:38 -0300

    create popup window
    add images, html and CSS

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ const stories = [
   {
     name: 'Multi-Post Stories',
     description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    featured_image: "./images/design.jpg",
+    description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featured_image1: "./images/design.jpg",
+    featured_image2:"./images/picturePopup.png",
     technologies: [
       'CSS',
       'html',
@@ -41,46 +43,52 @@ const stories = [
     ],
     live:'#',
     source:'#',
-},
-{
-  name: 'Multi-Post Stories',
-  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-  featured_image: "./images/design.jpg",
-  technologies: [
-    'CSS',
-    'html',
-    'bootstrap',
-    'ruby',
-  ],
-  live:'#',
-  source:'#',
-},
-{
-  name: 'Multi-Post Stories',
-  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-  featured_image: "./images/design.jpg",
-  technologies: [
-    'CSS',
-    'html',
-    'bootstrap',
-    'ruby',
-  ],
-  live:'#',
-  source:'#',
-},
-{
-  name: 'Multi-Post Stories',
-  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-  featured_image: "./images/design.jpg",
-  technologies: [
-    'CSS',
-    'html',
-    'bootstrap',
-    'ruby',
-  ],
-  live:'#',
-  source:'#',
-},
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featured_image1: "./images/design.jpg",
+    featured_image2:"./images/picturePopup.png",
+    technologies: [
+      'CSS',
+      'html',
+      'bootstrap',
+      'ruby',
+    ],
+    live:'#',
+    source:'#',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featured_image1: "./images/design.jpg",
+    featured_image2:"./images/picturePopup.png",
+    technologies: [
+      'CSS',
+      'html',
+      'bootstrap',
+      'ruby',
+    ],
+    live:'#',
+    source:'#',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
+    featured_image1: "./images/design.jpg",
+    featured_image2:"./images/picturePopup.png",
+    technologies: [
+      'CSS',
+      'html',
+      'bootstrap',
+      'ruby',
+    ],
+    live:'#',
+    source:'#',
+  },
 ];
 
 const workStory = document.querySelector('.work');
@@ -91,8 +99,8 @@ stories[0].technologies.forEach((technology) => {
 });
 for (let i = 0; i < stories.length; i += 1) {
 workStory.innerHTML += `
-      <div class="story"
-          <img class="picture" src=${stories[i].featured_image} alt="image about my story"/>
+      <div class="story">
+          <img class="picture" src=${stories[i].featured_image1} alt="image about my story"/>
           <div class="pictureDescription">
           <h3 class="post">${stories[i].name}</h3>
           <p class="postDescription">${stories[i].description}</p>
@@ -104,5 +112,5 @@ workStory.innerHTML += `
       `
 }
 const boton = document.querySelector('.project');
-const dataB = boton.dataset.id;
-console.log(dataB);
+const dataB = boton.dataset.id
+console.log(botonB)
