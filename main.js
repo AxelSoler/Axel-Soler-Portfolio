83bb2858a4e6ceb324220bda0ff6a2ef0b65c7dc
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-05 16:24:14 -0300

    work section is now created dynamically with JS

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ ham.addEventListener('click', () => {
   logo.classList.toggle('desactivate');
   logo2.classList.toggle('desactivate');
 });
+
 document.querySelectorAll('.menu_section').forEach((n) => n.addEventListener('click', () => {
   header.classList.remove('activate');
   ham.classList.remove('desactivate');
@@ -26,3 +27,127 @@ closeX.addEventListener('click', () => {
   logo.classList.remove('desactivate');
   logo2.classList.remove('desactivate');
 });
+
+const Stories = [
+  {
+    name: 'Multi-Post Stories',
+    description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    featured_image: "./images/design.jpg",
+    technologies: [
+      'CSS',
+      'html',
+      'bootstrap',
+      'ruby',
+    ],
+    live:'#',
+    source:'#',
+},
+{
+  name: 'Multi-Post Stories',
+  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  featured_image: "./images/design.jpg",
+  technologies: [
+    'CSS',
+    'html',
+    'bootstrap',
+    'ruby',
+  ],
+  live:'#',
+  source:'#',
+},
+{
+  name: 'Multi-Post Stories',
+  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  featured_image: "./images/design.jpg",
+  technologies: [
+    'CSS',
+    'html',
+    'bootstrap',
+    'ruby',
+  ],
+  live:'#',
+  source:'#',
+},
+{
+  name: 'Multi-Post Stories',
+  description:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  featured_image: "./images/design.jpg",
+  technologies: [
+    'CSS',
+    'html',
+    'bootstrap',
+    'ruby',
+  ],
+  live:'#',
+  source:'#',
+},
+];
+
+const workStory = document.querySelector('.work')
+
+workStory.innerHTML = `
+      <div class="story">
+          <img class="picture" src="./images/design.jpg" alt="image about my story"/>
+          <div class="pictureDescription">
+          <h3 class="post">Multi-Post Stories</h3>
+          <p class="postDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a standard dummy text.</p>
+          <ul class="code">
+              <li>CSS</li>
+              <li>html</li>
+              <li>bootstrap</li>
+              <li class="last">ruby</li>
+          </ul>
+          <button class="project" type="button">See Project</button>
+        </div>
+      </div>
+      <div class="story">
+          <img class="picture" src="./images/design.jpg" alt="image about my story"/>
+          <div class="pictureDescription">
+          <h3 class="post">Multi-Post Stories</h3>
+          <p class="postDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a standard dummy text.</p>
+          <ul class="code">
+              <li>CSS</li>
+              <li>html</li>
+              <li>bootstrap</li>
+              <li class="last">ruby</li>
+          </ul>
+          <button class="project" type="button">See Project</button>
+        </div>
+      </div>
+      <div class="story">
+          <img class="picture" src="./images/design.jpg" alt="image about my story"/>
+          <div class="pictureDescription">
+          <h3 class="post">Multi-Post Stories</h3>
+          <p class="postDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a standard dummy text.</p>
+          <ul class="code">
+              <li>CSS</li>
+              <li>html</li>
+              <li>bootstrap</li>
+              <li class="last">ruby</li>
+          </ul>
+          <button class="project" type="button">See Project</button>
+        </div>
+      </div>
+      <div class="story">
+          <img class="picture" src="./images/design.jpg" alt="image about my story"/>
+          <div class="pictureDescription">
+          <h3 class="post">Multi-Post Stories</h3>
+          <p class="postDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
+              has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a standard dummy text.</p>
+          <ul class="code">
+              <li>CSS</li>
+              <li>html</li>
+              <li>bootstrap</li>
+              <li class="last">ruby</li>
+          </ul>
+          <button class="project" type="button">See Project</button>
+        </div>
+      </div>
+      `
